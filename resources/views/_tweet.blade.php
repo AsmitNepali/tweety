e1842552f4de8e16687b150ecff4334968a7c505
Asmit Nepali <mail2asmitnepali@gmail.com>
2021-04-18 19:06:46 +0545

    Dymamic Template

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,11 +1,12 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50" alt="avtar" class="rounded-full mr-2">
+
+        <img src="{{auth()->user()->avatar}}" alt="avtar" class="rounded-full mr-2">
     </div>
     <div>
-        <h5 class="font-bold mb-4">John Doe</h5>
+        <h5 class="font-bold mb-4">{{$tweet->user->name}}</h5>
         <p class="text-sm">
-            In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.     
+            {{$tweet->body}}
         </p>
     </div>
 </div>
